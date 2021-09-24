e2991bdf4eef006eb14e631237bbb3af7e634b06
Amar Shankar <amarks444@gmail.com>
2021-09-24 12:05:06 +0530

    removed bug

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,4 +1,4 @@
-import React , { useState , useEffect , useRef } from 'react';
+import React , { useState , useEffect  } from 'react'; //, useRef
 import { useHistory } from 'react-router-dom';
 import { ChatEngine } from 'react-chat-engine';
 import { auth } from '../firebase';
